e7a43eacd36f051cdab27cbec36910d236fc48f8
LeeSeolHui <lsh81550@gmail.com>
2024-07-29 22:21:23 +0900

    feat: gin 프레임워크 도입

--- init/cmd/cmd.go
+++ init/cmd/cmd.go
@@ -1,21 +1,23 @@
 package cmd
 
 import (
-	"fmt"
 	"go-server/config"
+	"go-server/network"
 )
 
 // main은 cmd를 호출시킬 예정, 모든 항목들을 Cmd 스트럭쳐 안에 구성할 예정
 type Cmd struct {
-	config *config.Config
+	config  *config.Config
+	network *network.Network
 }
 
 func NewCmd(filePath string) *Cmd {
 	c := &Cmd{
-		config: config.NewConfig(filePath),
+		config:  config.NewConfig(filePath),
+		network: network.NewNetwork(),
 	}
 
-	fmt.Println(c.config.Server.Port)
+	c.network.ServerStart(c.config.Server.Port)
 
 	return c
 }
--- network/root.go
+++ network/root.go
@@ -1,3 +1,20 @@
 package network
 
+import "github.com/gin-gonic/gin"
+
 // API에 대해 라우터를 설정하는 부분이 될 것이다.
+type Network struct {
+	engine *gin.Engine
+}
+
+func NewNetwork() *Network {
+	// mux,echo,gin.. 프레임워크가 있는데, gin을 많이 주로 사용한다.
+	r := &Network{
+		engine: gin.New(), //New는 실제 프로덕트 환경에서, Default는 개발 환경에서
+	}
+	return r
+}
+
+func (n *Network) ServerStart(port string) error {
+	return n.engine.Run(port)
+}
