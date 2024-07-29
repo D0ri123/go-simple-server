f410f41c0c205bac7f1abc6e9806d03488934ff0
LeeSeolHui <lsh81550@gmail.com>
2024-07-29 21:41:27 +0900

    feature: 서버 동작 확인

--- init/main.go
+++ init/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/", helloWorld)
+
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		fmt.Println("에러가 떴다?")
+		panic(err)
+	}
+}
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("hello World")
 }
