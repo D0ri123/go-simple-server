c6f875bd1891c3838db15415e707b16c4c41f3c7
LeeSeolHui <lsh81550@gmail.com>
2024-07-29 22:02:53 +0900

    feat: config 파일 읽어들이도록 구현

--- config/config.go
+++ config/config.go
@@ -1,4 +1,27 @@
 package config
 
+import (
+	"github.com/naoina/toml"
+	"os"
+)
+
 // 서버의 기본적인 설정을 하는 파일
 // env -> toml
+type Config struct {
+	Server struct {
+		Port string
+	}
+}
+
+// config를 불러올 수 있는 함수
+func NewConfig(filePath string) *Config {
+	c := new(Config)
+
+	if file, err := os.Open(filePath); err != nil {
+		panic(err)
+	} else if err = toml.NewDecoder(file).Decode(c); err != nil {
+		panic(err)
+	} else {
+		return c
+	}
+}
